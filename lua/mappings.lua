289c582805ab7a39cc1b38ac364ddb1cc6b613dd
Oleksii Lisikh <o.lisikh@newmotion.com>
2023-04-10 19:12:15 +0200

    Some useful remaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,9 +12,28 @@ map('n', '<leader>o', ':NvimTreeToggle<CR>', { desc = 'nvim-tree: Toggle', norem
 map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Cursor in the middle while inserting
+map('n', 'J', 'mzJ`z')
+
+-- Move things between statements
+map('v', 'J', ":m '>+1<CR>gv=gv")
+map('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in the middle when moving half page up/down
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+
+-- Cursor in the middle during searches
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
+
+-- cut into the copy buffer and paste keeping the buffer
+map('x', '<leader>p', "\"_dP")
+
 -- Diagnostic keymaps
 map('n', '[d', vim.diagnostic.goto_prev, { desc = "go to previous diagnostic message" })
 map('n', ']d', vim.diagnostic.goto_next, { desc = "go to next diagnostic message" })
 map('n', '<leader>E', vim.diagnostic.open_float, { desc = "open floating diagnostic message" })
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = "open diagnostics list" })
+
 
