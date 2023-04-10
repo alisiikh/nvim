a0ac9237461c274a15ea1c34863d37759e597778
Oleksii Lisikh <o.lisikh@newmotion.com>
2023-04-10 15:05:15 +0200

    Fix on_attach for nvim-tree

--- init.lua
+++ init.lua
@@ -85,6 +85,7 @@ nvm_tree.setup {
       return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
 
+    api.config.mappings.default_on_attach(bufnr)
     -- TODO: add keymaps for nvim-tree
   end
 }
