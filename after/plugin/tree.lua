6e30fbc96dad1a735fe75f2091acfd320c43a587
Oleksii Lisikh <o.lisikh@newmotion.com>
2023-04-15 22:54:46 +0200

    Clean up nvim tree config

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,13 +1,7 @@
-local nvm_tree = require('nvim-tree')
-nvm_tree.setup {
+require('nvim-tree').setup {
   on_attach = function(bufnr)
     local api = require('nvim-tree.api')
-    local function opts(desc)
-      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
 
     api.config.mappings.default_on_attach(bufnr)
-    -- TODO: add keymaps for nvim-tree
   end
 }
-
