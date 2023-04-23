0752101304adb67d7df4bae35d61bcdababd8dce
Oleksii Lisikh <o.lisikh@newmotion.com>
2023-04-24 00:47:25 +0200

    Move lazy installation under plugins.lua

--- init.lua
+++ init.lua
@@ -1,28 +1,7 @@
---[[
---Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
---]]
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
+-- Note: this file is an entrypoint for neovim
 require('settings')
 require('plugins')
 require('mappings')
-
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
