1ef831338d4bf8ef4fa3926bc755e6c86d2e2397
Oleksii Lisikh <o.lisikh@newmotion.com>
2023-04-24 00:18:09 +0200

    Reverted git signs

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -2,10 +2,10 @@ local gsigns = require('gitsigns')
 
 gsigns.setup {
   signs = {
-    add          = { text = '❚' },
-    change       = { text = '❚' },
-    delete       = { text = '❚' },
-    topdelete    = { text = '❚' },
-    changedelete = { text = '❚' },
+    add          = { text = '+' },
+    change       = { text = '~' },
+    changedelete = { text = '~' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
   },
 }
